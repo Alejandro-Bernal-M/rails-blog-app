5d65e30b479685f0299ea21c01afc0abc83e07e4
alejandro <alejandrober96@gmail.com>
2023-06-06 14:41:56 -0500

    Create post table with foreign key author_id

---
+++ db/migrate/20230606193323_add_author_id_to_posts.rb
@@ -0,0 +1,6 @@
+class AddAuthorIdToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :author_id, :integer
+    add_foreign_key :posts, :users, column: :author_id
+  end
+end

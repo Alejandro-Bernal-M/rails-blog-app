f662d4d8e59c052e1f1b6c80399310d48657b51d
alejandro <alejandrober96@gmail.com>
2023-06-06 14:56:17 -0500

    Create Likes table with foreigns keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_193949) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_194627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "Title"
@@ -33,5 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_193949) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end

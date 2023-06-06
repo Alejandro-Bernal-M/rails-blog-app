2e1d111c7561c0126c8eb7ff0f587d0ea8c9e0a0
alejandro <alejandrober96@gmail.com>
2023-06-06 15:01:57 -0500

    Add comments table with foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_194627) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_200033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
+    t.text "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "author_id"
@@ -40,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_194627) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"

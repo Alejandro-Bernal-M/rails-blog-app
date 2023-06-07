3de46b4a8fc92f357361ee863d85d448397db0ca
alejandro <alejandrober96@gmail.com>
2023-06-07 14:16:09 -0500

    space correnctions

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  #A method that updates the posts counter for a user.
+  # A method that updates the posts counter for a user.
   def update_posts_counter
     self.author.update(PostCounter: self.author.posts.count)
+  end
+
+  # A method which returns the 5 most recent comments for a given post.
+  def return_five_most_recent_comments
+    self.comments.order(created_at: :desc).limit(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
-  #A method that returns the 3 most recent posts for a given user.
+  # A method that returns the 3 most recent posts for a given user.
   def return_three_most_recent_posts
     self.posts.order(created_at: :desc).limit(3)
   end

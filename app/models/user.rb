b59d3fa0d49ffd404d86dc16be6813ff4ac0d393
alejandro <alejandrober96@gmail.com>
2023-06-08 13:18:20 -0500

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
+  # Validations
+  validates :Name, presence: true, length: { minimun: 3, maximum: 20}
+  validates :PostCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # A method that returns the 3 most recent posts for a given user.
   def return_three_most_recent_posts
     posts.order(created_at: :desc).limit(3)

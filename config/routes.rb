1b9ac399e8b0b0788323da589e78b60eaed17eec
alejandro <alejandrober96@gmail.com>
2023-06-14 19:04:19 -0500

    corrections

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'users#index'
-  get '*path', to: 'users#invalid_user'
   get '/users', to: 'users#index', as: 'users'
-
+  
   get '/users/:id', to: 'users#show', as: 'user'
-
+  
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_user_post'
   post '/users/:user_id/posts', to: 'posts#create'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   get '/users/:user_id/posts/:post_id/new', to: 'comments#new', as: 'new_comment'
   post '/users/:user_id/posts/:post_id', to: 'comments#create', as: 'create_comment'
-
+  
   post '/users/:user_id/posts/:post_id/likes', to: 'likes#create', as: 'create_like'
+  get '/users/*path', to: 'users#invalid_user', constraints: { path: /[^\/]+/ }
 end
